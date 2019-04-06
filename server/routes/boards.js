29962a3a01a5e20d23a12b5cea210e41c6ddbfc2
gbarrbrown <gbb7899@gmail.com>
2019-04-06 22:42:26 +1300

    Setup server and boards route

---
+++ server/routes/boards.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+// GET /api/v1/boards
+router.get('/', (req, res) => {
+
+})
+
+// POST /api/v1/boards
+router.post('/', (req, res) => {
+
+})
+
+module.exports = router
