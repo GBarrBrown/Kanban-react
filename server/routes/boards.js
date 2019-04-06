df086942d5ae7db32ed20d295c08724d27b31309
gbarrbrown <gbb7899@gmail.com>
2019-04-06 23:22:23 +1300

    Setup boards route to return all boards from db

--- server/routes/boards.js
+++ server/routes/boards.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const { getBoards } = require('../db/boards')
+
 // GET /api/v1/boards
 router.get('/', (req, res) => {
-
+    getBoards()
+    .then((boards) => {
+        res.json(boards)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({error: 'Something went wrong'})
+    })
 })
 
 // POST /api/v1/boards
