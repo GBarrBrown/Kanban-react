1eed7ae5412a568dc31122f181be769bc14e5443
jesse collier <jesse.collier97@gmail.com>
2019-04-26 22:59:50 +1200

    added migrations

--- migrations/20190406224259_boards.js
+++ migrations/20190406224259_boards.js
@@ -3,8 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('boards', table => {
         table.increments('id').primary()
         table.string('name')
-        table.string('description')
-        table.string('columns').defaultTo('TODO, DOING, DONE')
+        table.integer('accountId')
     })
 };
 
