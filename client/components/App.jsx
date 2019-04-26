54b233a09cc9603a245e9bbdd8618858d209ed1f
jesse collier <jesse.collier97@gmail.com>
2019-04-26 22:26:54 +1200

    added boards.js function names, not many done properly yet

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component, Fragment} from 'react';
+import { connect } from "react-redux";
+import {} from 'react-router-dom'
 
-const App = () => {
-    return(
-        <h1>Hello, world!</h1>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <Fragment>
+                
+            </Fragment>
+        );
+    }
 }
-
-export default App
+ 
+export default App;
--- server/db/boards.js
+++ server/db/boards.js
@@ -9,3 +9,57 @@ function getBoards(testDb) {
 
     return db('boards')
 }
+
+function createBoard(board, testDb){
+    const db = testDb || connection;
+
+    return;
+}
+
+function getBoard(boardId, testDb) {
+    const db = testDb || connection
+
+    return db('boards').where(id, boardId);
+}
+
+function createColumn(testDb) {
+    const db = testDb || connection
+
+    return db('boards')
+}
+
+function editColumn(testDb) {
+    const db = testDb || connection
+
+    return db('boards')
+}
+
+function deleteColumn(testDb) {
+    const db = testDb || connection
+
+    return db('boards')
+}
+
+function addCard(testDb) {
+    const db = testDb || connection
+
+    return db('boards')
+}
+
+function editCard(testDb) {
+    const db = testDb || connection
+
+    return db('boards')
+}
+
+function deleteCard(testDb) {
+    const db = testDb || connection
+
+    return db('boards')
+}
+
+function moveCard(testDb) {
+    const db = testDb || connection
+    //moves card from one column to another
+    return db('boards')
+}
